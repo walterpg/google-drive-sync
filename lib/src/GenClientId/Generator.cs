d62e5ad0cd60d1ec43f3343b176c9fea2611cc44
walterpg1v <15221668+walterpg@users.noreply.github.com>
2020-04-26 21:44:12 -0500

    New tool to produce for generating KP-obfuscated creds from text.

---
+++ lib/src/GenClientId/Generator.cs
@@ -0,0 +1,83 @@
+﻿using KeePassLib.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenClientId
+{
+    public class Generator
+    {
+        static string GenSrc(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (0 == i % 8)
+                {
+                    sb.Append("            ");
+                }
+                sb.AppendFormat("0x{0:x2},", bytes[i]);
+                if (0 != (i + 1) % 8 &&
+                    (i + 1) < bytes.Length)
+                {
+                    sb.Append(' ');
+                }
+                else
+                {
+                    sb.Append(Environment.NewLine);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public Generator()
+        {
+        }
+
+        public string SourceFilePath { get; set; }
+
+        public string ClientIdCsharpConstant { get; set; }
+        public string ClientIdPadCsharpConstant { get; set; }
+        public string ClientSecretCsharpConstant { get; set; }
+        public string ClientSecretPadCsharpConstant { get; set; }
+
+        byte[] ClientIdBytes { get; set; }
+        byte[] ClientIdPadBytes { get; set; }
+        byte[] ClientSecretBytes { get; set; }
+        byte[] ClientSecretPadBytes { get; set; }
+
+        public void Run()
+        {
+            using (StreamReader reader = File.OpenText(SourceFilePath))
+            {
+                ClientIdBytes = Encoding.UTF8.GetBytes(
+                                    reader.ReadLine().Trim());
+                ClientSecretBytes = Encoding.UTF8.GetBytes(
+                                    reader.ReadLine().Trim());
+                ClientIdPadBytes = new byte[ClientIdBytes.Length];
+                ClientSecretPadBytes = new byte[ClientSecretBytes.Length];
+
+                using (RandomNumberGenerator rng = 
+                            RandomNumberGenerator.Create())
+                {
+                    rng.GetNonZeroBytes(ClientIdPadBytes);
+                    rng.GetNonZeroBytes(ClientSecretPadBytes);
+                }
+
+                XorredBuffer clientIdBuf = new XorredBuffer(ClientIdBytes,
+                                                            ClientIdPadBytes);
+                XorredBuffer secretBuf = new XorredBuffer(ClientSecretBytes,
+                                                        ClientSecretPadBytes);
+
+                ClientIdCsharpConstant = GenSrc(clientIdBuf.ReadPlainText());
+                ClientIdPadCsharpConstant = GenSrc(ClientIdPadBytes);
+                ClientSecretCsharpConstant = GenSrc(secretBuf.ReadPlainText());
+                ClientSecretPadCsharpConstant = GenSrc(ClientSecretPadBytes);
+            }
+        }
+    }
+}
---
+++ lib/src/GenClientId/Program.cs
@@ -0,0 +1,74 @@
+﻿using KeePassLib.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace GenClientId
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FOR TESTING PURPOSES ONLY.
+
+            // Two arguments:
+            // 1. The path of a source file.
+            // 2. The path of a target file.
+            //
+            // The UTF8-encoded source file must contain two lines of text:
+            // 1. An OAuth2.0 client ID.
+            // 2. The cleartext secret associated with the client ID
+            //
+
+            Generator gen = new Generator()
+            {
+                SourceFilePath = args[0]
+            };
+            gen.Run();
+
+            StringBuilder src = new StringBuilder(@"
+namespace GoogleDriveSync
+{
+    static partial class GdsDefs
+    {
+        static readonly byte[] s_clientIdBytes = new byte[]
+        {
+");
+            src.Append(gen.ClientIdCsharpConstant);
+            src.Append(@"
+        };
+        static readonly byte[] s_clientIdPad = new byte[]
+        {
+");
+            src.Append(gen.ClientIdPadCsharpConstant);
+            src.Append(@"
+        };
+        static readonly byte[] s_clientSecretBytes = new byte[]
+		{
+");
+            src.Append(gen.ClientSecretCsharpConstant);
+            src.Append(@"
+		};
+		static readonly byte[] s_clientSecretPad = new byte[]
+		{
+");
+            src.Append(gen.ClientSecretPadCsharpConstant);
+            src.Append(@"
+		};
+	}
+}");
+
+            Debug.WriteLine(src.ToString());
+
+            using (StreamWriter sw = File.CreateText(args[1]))
+            {
+                sw.Write(src.ToString());
+            }
+        }
+    }
+}
