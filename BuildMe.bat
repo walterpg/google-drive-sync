0689b193d3b2261e8b19a99ca28169d1933ff13d
walterpg1v <15221668+walterpg@users.noreply.github.com>
2020-05-10 13:45:17 -0500

    Build dist/readme.txt with pandoc

--- BuildMe.bat
+++ BuildMe.bat
@@ -1,7 +1,8 @@
 @echo off
 set version=4.0.0-alpha
-set sevenzip="C:\Program Files (x86)\7-Zip\7z.exe"
-set msbuildcmd="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
+set sevenzip="%ProgramFiles(x86)%\7-Zip\7z.exe"
+set msbuildcmd="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
+set pandoc="%LOCALAPPDATA%\Pandoc\pandoc.exe"
 
 if not exist %msbuildcmd% goto errmsbuild
 if not exist %sevenzip% goto errsevenzip
@@ -23,9 +24,9 @@ if %errorlevel% NEQ 0 goto error
 
 xcopy src\bin\Release\*.* build\bin
 copy src\bin\Release\GoogleDriveSync.plgx build\dist\GoogleDriveSync-%version%.plgx
-copy doc\readme.txt build\dist
 %sevenzip% a -tzip build\dist\GoogleDriveSync-%version%.zip .\build\bin\*.dll .\build\bin\*.pdb
 .\lib\src\GenVerInfo\bin\Release\GenVerInfo.exe .\build\bin\GoogleDriveSync.dll .\current_version_manifest.txt
+%pandoc% -f gfm -t plain --wrap=auto doc\README.md -o build\dist\readme.txt
 
 goto end
 
