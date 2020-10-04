776bc61168d37ed73e9c3bc6f64211d7296680e5
walterpg1v <15221668+walterpg@users.noreply.github.com>
2020-10-04 17:18:38 -0500

    Code review cleanup.

--- src/FolderHelpers.cs
+++ src/FolderHelpers.cs
@@ -1,11 +1,32 @@
-﻿using Google.Apis.Drive.v3;
+﻿/**
+ * Google Sync Plugin for KeePass Password Safe
+ * Copyright(C) 2012-2016  DesignsInnovate
+ * Copyright(C) 2014-2016  Paul Voegler
+ * 
+ * KeePass Sync for Google Drive
+ * Copyright(C) 2020       Walter Goodwin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using GDriveFile = Google.Apis.Drive.v3.Data.File;
