ea444b0f0de47e4e07fe79ee3e22f4d6317f35c6
walterpg1v <15221668+walterpg@users.noreply.github.com>
2021-02-01 14:11:36 -0600

    Prepare new release.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.0.5.0")]
 [assembly: AssemblyFileVersion("4.0.5.0")]
-[assembly: AssemblyInformationalVersion("4.0.5-unstable")]
+[assembly: AssemblyInformationalVersion("4.0.5-beta")]
