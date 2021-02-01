8e977e553bbbbafd59ef1e9bea363b0929db1d0f
walterpg1v <15221668+walterpg@users.noreply.github.com>
2021-02-01 14:23:59 -0600

    Post-release updates.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("966575FC-0771-438d-B406-27C2E231F902")]
 
-[assembly: AssemblyVersion("4.0.5.0")]
-[assembly: AssemblyFileVersion("4.0.5.0")]
-[assembly: AssemblyInformationalVersion("4.0.5-beta")]
+[assembly: AssemblyVersion("4.0.6.0")]
+[assembly: AssemblyFileVersion("4.0.6.0")]
+[assembly: AssemblyInformationalVersion("4.0.6-unstable")]
